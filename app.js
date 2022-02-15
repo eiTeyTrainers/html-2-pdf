dceb9fdce9f89fc67eebe070baaed0816756032f
eiTeyTrainers <eiTeytrainers@gmail.com>
2022-02-15 23:07:50 +0200

    Update: calc() + app.js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 
 function downloadpdf(){
-    var element = document.getElementById("document")
-    opt = {
-      filename: 'myfile.pdf',
-      image: {type: 'jpeg',quality: 1},
-    }
-    
-    var link = document.getElementById("id-css");
-    link.setAttribute("href", "css/test-pdf.css");
-    //change
-    html2pdf(element)
-    
-     //change back
-    link.setAttribute("href", "css/test.css");
-    //change back
+  var element = document.getElementById("document")
+  opt = {
+    filename: document.title + '.pdf',
+    image: {type: 'jpeg',quality: 1},
+  }
+  
+  var link = document.getElementById("id-css");
+  
+  var body = document.getElementById('id-body');
+  link.setAttribute("href", "css/pdf.css");
+  //change
+  html2pdf(element, opt)
+  body.style.color = "white";
+  link.setAttribute("href", "css/html.css")
+  const myTimeout = setTimeout(changeColor, 8);
+}
+
+function changeColor() {
+var body = document.getElementById('id-body');
+
+body.style.color = "black";
 }
 
