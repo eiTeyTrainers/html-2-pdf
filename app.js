6f475fe2cc42ecd3b0414eb35d27c4832035254e
eiTeyTrainers <eiTeytrainers@gmail.com>
2022-02-17 00:20:11 +0200

    .

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var percentage = 30;
+var percentage = 1;
 var root = document.documentElement;
 root.style.cssText = '--percentage : ' + percentage;
-document.getElementById('percentage').innerHTML = "percentage";
+function loadPercentage(){
+  document.getElementById("percentage").innerHTML = percentage;
+}
+
 function downloadpdf(){
   var element = document.getElementById("document")
   opt = {
@@ -12,17 +15,22 @@ function downloadpdf(){
   var link = document.getElementById("id-css");
   
   var body = document.getElementById('id-body');
+  var downloadHider = document.getElementById('hide-download');
   link.setAttribute("href", "css/pdf.css");
   //change
   html2pdf(element, opt)
   body.style.color = "white";
+  downloadHider.style.display = 'block'
+  
   link.setAttribute("href", "css/html.css")
-  const myTimeout = setTimeout(changeColor, 8);
+  const myTimeout = setTimeout(changeColor, 200);
 }
 
 function changeColor() {
-var body = document.getElementById('id-body');
-
-body.style.color = "black";
+  var body = document.getElementById('id-body');
+  var downloadHider = document.getElementById('hide-download');
+  
+  downloadHider.style.display = 'none'
+  body.style.color = "black";
 }
 
