572d51bfe4f474b9d3a5a576a98970030561c4bb
eiTeyTrainers <eiTeytrainers@gmail.com>
2022-02-16 17:10:44 +0200

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var percentage = 100;
+var percentage = 30;
 var root = document.documentElement;
 root.style.cssText = '--percentage : ' + percentage;
-
+document.getElementById('percentage').innerHTML = "percentage";
 function downloadpdf(){
   var element = document.getElementById("document")
   opt = {
