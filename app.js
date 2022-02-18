0d277be934fd8bca95011209a6f83fb4d20b6303
eiTeyTrainers <eiTeytrainers@gmail.com>
2022-02-19 01:49:37 +0200

    .

--- app.js
+++ app.js
@@ -9,17 +9,18 @@ function downloadpdf(){
   var element = document.getElementById("document")
   var css = document.getElementById("id-css");
   var body = document.getElementById('id-body');
+  var downloadHider = document.getElementById('hide-download');
   opt = {
     filename: document.title + '.pdf',
     image: {type: 'jpeg',quality: 1},
-    html2canvas:  { scale: 1 , useCORS: true},
+    html2canvas:  { scale: 2 , useCORS: true},
     jsPDF: { format: 'A4', orientation: 'portrait' },
     
   }
   
-  var downloadHider = document.getElementById('hide-download');
-  //change
+  css.setAttribute("href", "css/pdf.css");
   html2pdf().set(opt).from(element).then(function(){
+    css.setAttribute("href", "css/html.css");
 
   }).save()
 
